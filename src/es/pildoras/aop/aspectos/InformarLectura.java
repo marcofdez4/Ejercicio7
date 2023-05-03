6a5e54aebc402a5ad4210343fa746b765ce0650d
marcofdez4 <marcosfdezglez4@gmail.com>
2023-05-03 11:38:49 +0200

    ejercicio 7 con after

--- src/es/pildoras/aop/aspectos/InformarLectura.java
+++ src/es/pildoras/aop/aspectos/InformarLectura.java
@@ -1,5 +1,6 @@
 package es.pildoras.aop.aspectos;
 
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
@@ -12,5 +13,11 @@ public class InformarLectura {
 	public void estaPorInformar()
 	{
 		System.out.println("Se va a procesar la lectura del fichero...");
+	}
+	
+	@After("execution (public void lecturaFichero())")
+	public void finDeLectura()
+	{
+		System.out.println("Fin del informe, pase un buen día");
 	}
 }
